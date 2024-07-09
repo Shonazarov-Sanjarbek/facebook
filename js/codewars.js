baf0bbcea236e94e4e237a4bb16ecf5a2d20e350
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-07-09 19:18:31 +0500

    homework

---
+++ js/codewars.js
@@ -0,0 +1,96 @@
+{
+    // function countSheeps(sheep) {
+    
+    //     let ship = 0
+    //     if (sheep) {
+    //         ship++
+    //         return ship
+    //     }
+    // }
+    
+    //   console.log(countSheeps([undefined,null,false,true])); // 1
+}
+
+{
+    // function numberToString(num) {
+    //     return num.toString()
+    //   }
+    //   console.log(numberToString(67));
+    //   console.log(numberToString(300));
+}
+
+{
+    // function solution(str){
+    //     return str.split("").reverse().join("")
+    //   }
+    //   console.log(world); //dlrow
+}
+
+{
+    // function opposite(number) {
+    //     if(number === 0){
+    //         return 0
+    //     }else if (number < 0){
+    //         return -number
+    //     }else{
+    //         return number / -1
+    //     }
+    // }
+
+    // console.log(opposite(1));  //-1
+    // console.log(opposite(0));  //0
+    // console.log(opposite(-1));  //1
+}
+
+{
+    // function makeNegative(num) {
+    //         if(num === 0){
+    //         return 0
+    //     }else if (num > 0){
+    //         return -num
+    //     }else{
+    //         return num
+    //     }
+    // }
+
+    // console.log(makeNegative(42));  //-42
+    // console.log(makeNegative(0));  //0
+    // console.log(makeNegative(-9));  //-9
+}
+
+{
+    // function boolToWord( bool ){
+    //     if (bool) {
+    //         return `Yes`
+    //     }else{
+    //         return `No`
+    //     }
+    //   }
+    //   console.log(boolToWord(true)); //Yes
+    //   console.log(boolToWord(false)); // NO
+}
+
+{
+    // function positiveSum(arr) {
+    //     let sum = 0
+    //     for (let i = 0; i < arr.length; i++){
+    //         if (arr[i] > 0) {
+    //             sum += arr[i]
+    //         }
+    //     }
+    //     return sum
+    // }
+    // console.log(positiveSum([1,2,3,4,5]));  // 15
+    // console.log(positiveSum([-1,-2,-3,-4,-5])); // 13
+    // console.log(positiveSum([-1,-2,-3,-4,-5])); // 0
+    // console.log(positiveSum([-1,2,3,4,-5])); // 9
+
+}
+
+{
+    function repeatStr (n, s) {
+        return s.repeat(n)
+      }
+      console.log(repeatStr(3, "*"));  //*** 
+      console.log(repeatStr(5, "#"));  //#####  
+}
