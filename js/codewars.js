ed85b26b52cdfa38a9d72588b7cc5133fe026677
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-07-09 21:39:45 +0500

    homework-16

--- js/codewars.js
+++ js/codewars.js
@@ -84,13 +84,108 @@
     // console.log(positiveSum([-1,-2,-3,-4,-5])); // 13
     // console.log(positiveSum([-1,-2,-3,-4,-5])); // 0
     // console.log(positiveSum([-1,2,3,4,-5])); // 9
-
 }
 
 {
-    function repeatStr (n, s) {
-        return s.repeat(n)
+    // function repeatStr (n, s) {
+    //     return s.repeat(n)
+    //   }
+    //   console.log(repeatStr(3, "*"));  //*** 
+    //   console.log(repeatStr(5, "#"));  //#####  
+    //   console.log(repeatStr(4, "salom"));  //#####  
+}
+
+{
+    // function removeChar(str){
+    //     return str.slice(1, -1)
+    // }
+    // console.log(removeChar("eloquent")); //loquen
+    // console.log(removeChar("country")); //ountr
+    // console.log(removeChar("person")); //erso
+}
+
+{
+    // function squareSum(numbers){
+    //     let sum = 0
+    //     for (let i = 0; i < numbers.length; i++) {
+    //         sum += numbers[i] ** 2
+    //     }
+    //     return sum
+    // }
+    // console.log(squareSum ([1, 2])); // 5
+    // console.log(squareSum ([0, 3, 4, 5])); // 50
+}
+
+{
+    // function findSmallestInt(arr) {
+    //     arr.sort((a,b) => a-b)
+    //     return arr[0]
+    // }
+    // console.log(findSmallestInt([78,56,232,12,8])); // 8
+    // console.log(findSmallestInt([78,56,232,12,18])); // 12
+    // console.log(findSmallestInt([78,56,232,412,228])); // 56
+}
+
+{
+    // const stringToNumber = function(str){
+    //     return +str
+    // }
+    // console.log(stringToNumber("1234")); // 1234
+    // console.log(stringToNumber("605")); // 605
+    // console.log(stringToNumber("1405")); // 1405
+    // console.log(stringToNumber("-7")); // -7
+}
+
+{
+    // var summation = function (num) {
+    //     let sum = 0
+    //     for (let i = 0; i <= num; i++) {
+    //         sum += i            
+    //     }
+    //     return sum
+    //   }
+    //   console.log(summation(1)); // 1
+    //   console.log(summation(2)); // 3
+    //   console.log(summation(8)); // 36
+}
+
+{
+    // function greet(str) {
+    //    return  `hello world!` 
+    // }
+    // console.log(greet()); // hello world!
+}
+
+{
+    // function noSpace(x){
+    //     return x.replaceAll(" ", "")
+    // }
+
+    // console.log(noSpace(`8 j 8   mBliB8g  imjB8B8  jl  B`)); //8j8mBliB8gimjB8B8jlB
+    // console.log(noSpace(`8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd`)); //88Bifk8hB8BB8BBBB888chl8BhBfd
+    // console.log(noSpace(`8aaaaa dddd r     `)); //8aaaaaddddr
+}
+
+{
+    // function doubleInteger(i) {
+    //     return i * 2
+    //   }
+    //   console.log(doubleInteger(2)); // 4
+    //   console.log(doubleInteger(12)); // 24
+    //   console.log(doubleInteger(13)); // 26
+}
+
+{
+    // function greet(name){
+    //     return `Hello, ${name} how are you doing today?`
+    // }
+    // console.log(greet(`Ryan`)); // "Hello, Ryan how are you doing today?"
+}
+
+{
+    function booleanToString(b){
+        return b.toString()
       }
-      console.log(repeatStr(3, "*"));  //*** 
-      console.log(repeatStr(5, "#"));  //#####  
+      console.log(booleanToString(true)); // true
+      console.log(booleanToString(false)); // true
 }
